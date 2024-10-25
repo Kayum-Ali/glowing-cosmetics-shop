289a588b6fdae430a8d5314be686babe8494020f
Kayum <sisirfahimstudent173@gmail.com>
2024-10-25 23:26:25 +0600

    about us healthy glow section done

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -11,7 +11,12 @@ const AboutUsPage = () => {
         </div>
       </div>
 
-      
+      {/* about us healthy glow */}
+      <div className="text-center  lg:my-20 my-14 space-y-3 px-10">
+          <img className="mx-auto" src="https://res.cloudinary.com/dqescabbl/image/upload/v1729876367/image-box-11_z9ykt6.jpg" alt="" />
+          <h2 className="lg:text-4xl text-3xl text-gray-800 font-bold mx-auto lg:w-[600px]">We strive to live with compassion, kindness and empathy</h2>
+          <p className="lg:w-1/2 mx-auto  text-gray-800 opacity-80 font-medium">A lot of so-called stretch denim pants out there are just glorified sweatpants – they get baggy and lose their shape. Not cool. Our tightly knitted fabric holds its form after repeated wear. Plus, Aldays dress up or down, no prob. So you can wear them all day. Get it?</p>
+      </div>
     </div>
   );
 };
