5f6b5d6884970a30f633ff63938db42565398ffb
Kayum <sisirfahimstudent173@gmail.com>
2024-10-26 18:11:00 +0600

    getin touch section done

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,3 +1,6 @@
+import { FaLocationDot } from "react-icons/fa6";
+import { LuPhoneCall } from "react-icons/lu";
+import { MdAccessTime } from "react-icons/md";
 import { Link } from "react-router-dom";
 
 const ContactUs = () => {
@@ -22,6 +25,50 @@ const ContactUs = () => {
             </div>
       </div>
       {/* get in touch section */}
+      <div className="text-center text-gray-800 py-16 space-y-4">
+          <h2 className="text-5xl font-bold">  Keep In Touch with Us</h2>
+          <p className="text-xl font-medium opacity-80">We’re talking about clean beauty gift sets, of course – and we’ve got a <br /> bouquet of beauties for yourself or someone you love.</p>
+      </div>
+
+      <div className="flex container mx-auto justify-evenly gap-10">
+        {/* 1st */}
+          <div className="flex gap-4">
+              <div>
+                 <FaLocationDot className="text-4xl" />
+              </div>
+              <div className="text-gray-800 space-y-3">
+                <h2 className="text-3xl font-bold">Address</h2>
+                <p className="font-medium opacity-80 text-xl">3245 Abbot Kinney BLVD - <br /> PH Venice, CA 124</p>
+                <p className="font-medium opacity-80 text-xl">Saidpur, Rangpur Division, Bangladesh.</p>
+              </div>
+          </div>
+          {/* 2nd */}
+          <div className="flex gap-4">
+             <div>
+                 <LuPhoneCall className="text-4xl"  />
+             </div>
+             <div className="text-gray-800 space-y-3"> 
+                <h2 className="text-3xl font-bold">Contact</h2>
+                <p className="font-medium text-xl opacity-80">Mobile : <span className="text-black font-bold">01867217084</span></p>
+                <p className="font-medium text-xl opacity-80">Hotline: <span className="text-black font-bold">01779596173</span></p>
+                <p className="font-medium text-xl opacity-80">Email: <span className="text-black font-bold">loge_asi@gmail.com</span></p>
+             </div>
+          </div>
+          {/* 3rd */}
+          <div className="flex gap-4 ">
+            <div>
+              <MdAccessTime className="text-4xl"/>
+            </div>
+            <div className="text-gray-800 space-y-3">
+                <h2 className="text-3xl font-bold">Hour of operation</h2>
+                <div className="space-y-3">
+                     <p className="font-medium text-xl text-black">Monday to Friday : <span className="opacity-80"> 08:00 AM - 18:00 PM</span></p>
+                     <p className="font-medium text-xl text-black">Saturday & Sunday : <span className="opacity-80"> 09:00 AM - 21:00 PM</span></p>
+ 
+                </div>
+            </div>
+          </div>
+      </div>
 
 
     </div>
