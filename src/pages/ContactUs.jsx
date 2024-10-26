ad082dcb2684f56c849ce854e41406c7e6ca303d
Kayum <sisirfahimstudent173@gmail.com>
2024-10-26 19:13:59 +0600

    google map added

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -30,7 +30,7 @@ const ContactUs = () => {
           <p className="text-xl font-medium opacity-80">We’re talking about clean beauty gift sets, of course – and we’ve got a <br /> bouquet of beauties for yourself or someone you love.</p>
       </div>
 
-      <div className="flex container mx-auto justify-evenly gap-10">
+      <div className="flex container my-10 pb-5 mx-auto justify-evenly gap-10">
         {/* 1st */}
           <div className="flex gap-4">
               <div>
@@ -68,6 +68,12 @@ const ContactUs = () => {
                 </div>
             </div>
           </div>
+      </div>
+   
+
+      {/* google map */}
+      <div className="my-5">
+        <iframe className="w-4/5 h-[450px] mx-auto rounded-lg" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3251.16040383638!2d88.89368367485659!3d25.78214830776096!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39e3510c25d5cb7d%3A0xdc031227e1d15836!2sSaidpur%20Plaza!5e1!3m2!1sen!2sbd!4v1729948180682!5m2!1sen!2sbd" allowfullscreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
       </div>
 
 
