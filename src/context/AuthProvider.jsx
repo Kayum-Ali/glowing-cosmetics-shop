4c66094b8bffdbbbf046705f4f2eeb52345ad0ae
Kayum Ali <sisirfahimstudent173@gmail.com>
2024-10-31 23:56:10 +0600

    sign up and login user added

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,20 +1,41 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { createContext } from "react";
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
 import auth from "../config/firebase.config";
 import PropTypes from "prop-types";
 
 export const AuthContext = createContext()
 
 const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null)
     const googleProvider = new GoogleAuthProvider()
-    
+
     const googleLogin = async() =>{
         return signInWithPopup(auth,googleProvider)
     }
+
+    const signUp = async( email, password) =>{
+        return await createUserWithEmailAndPassword(auth,email, password)
+    }
+    const login = async(email, password) =>{
+        return await signInWithEmailAndPassword(auth,email,password)
+    }
     const authInfo = {
-        googleLogin
+        googleLogin,
+        signUp,
+        user
 
     }
+
+
+    useEffect(()=>{
+        const unSubsCribed = onAuthStateChanged(auth, (currentUser)=>{
+            console.log(currentUser);
+            setUser(currentUser)
+        })
+        return ()=>{
+            unSubsCribed()
+        }
+    },[])
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
