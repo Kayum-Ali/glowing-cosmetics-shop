a66854f85dd7e900461d8d8e01e64a8938ca8aa8
programmerhasan0 <programmerhasan0@gmail.com>
2024-11-05 00:38:36 +0600

    updated firebase.config.js

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -1,17 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import credintials from "./credentials";
+import credentials from "./credentials";
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_apiKey || credintials.VITE_apiKey,
-    authDomain: import.meta.env.VITE_authDomain || credintials.VITE_authDomain,
-    projectId: import.meta.env.VITE_projectId || credintials.VITE_projectId,
+    apiKey: import.meta.env.VITE_apiKey || credentials.VITE_apiKey,
+    authDomain: import.meta.env.VITE_authDomain || credentials.VITE_authDomain,
+    projectId: import.meta.env.VITE_projectId || credentials.VITE_projectId,
     storageBucket:
-        import.meta.env.VITE_storageBucket || credintials.VITE_storageBucket,
+        import.meta.env.VITE_storageBucket || credentials.VITE_storageBucket,
     messagingSenderId:
         import.meta.env.VITE_messagingSenderId ||
-        credintials.VITE_messagingSenderId,
-    appId: import.meta.env.VITE_appId || credintials.VITE_appId,
+        credentials.VITE_messagingSenderId,
+    appId: import.meta.env.VITE_appId || credentials.VITE_appId,
 };
 
 const app = initializeApp(firebaseConfig);
