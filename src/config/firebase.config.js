3656b25fefe8533408ef5e7aee0e432d13c560a5
programmerhasan0 <programmerhasan0@gmail.com>
2024-11-05 00:28:02 +0600

    modified handleAddToCart

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -1,19 +1,19 @@
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import credintials from "./credintials";
 
 const firebaseConfig = {
-  apiKey:import.meta.env.VITE_apiKey,
-  authDomain:import.meta.env.VITE_authDomain,
-  projectId:import.meta.env.VITE_projectId,
-  storageBucket:import.meta.env.VITE_storageBucket,
-  messagingSenderId:import.meta.env.VITE_messagingSenderId,
-  appId:import.meta.env.VITE_appId
+    apiKey: import.meta.env.VITE_apiKey || credintials.VITE_apiKey,
+    authDomain: import.meta.env.VITE_authDomain || credintials.VITE_authDomain,
+    projectId: import.meta.env.VITE_projectId || credintials.VITE_projectId,
+    storageBucket:
+        import.meta.env.VITE_storageBucket || credintials.VITE_storageBucket,
+    messagingSenderId:
+        import.meta.env.VITE_messagingSenderId ||
+        credintials.VITE_messagingSenderId,
+    appId: import.meta.env.VITE_appId || credintials.VITE_appId,
 };
 
-console.log(import.meta.env.VITE_apiKey);
-
-
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app)
+const auth = getAuth(app);
 export default auth;
