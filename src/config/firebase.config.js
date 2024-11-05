14b6a446aeb5ffc3d860f76287f3e4992d2b6dc8
Kayum Ali <sisirfahimstudent173@gmail.com>
2024-11-05 20:44:35 +0600

    addtocart table done

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -1,17 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import credentials from "./credentials";
+// import credentials from "./credentials";
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_apiKey || credentials.VITE_apiKey,
-    authDomain: import.meta.env.VITE_authDomain || credentials.VITE_authDomain,
-    projectId: import.meta.env.VITE_projectId || credentials.VITE_projectId,
-    storageBucket:
-        import.meta.env.VITE_storageBucket || credentials.VITE_storageBucket,
-    messagingSenderId:
-        import.meta.env.VITE_messagingSenderId ||
-        credentials.VITE_messagingSenderId,
-    appId: import.meta.env.VITE_appId || credentials.VITE_appId,
+    apiKey: import.meta.env.VITE_apiKey,
+    authDomain: import.meta.env.VITE_authDomain,
+    projectId: import.meta.env.VITE_projectId,
+    storageBucket:import.meta.env.VITE_storageBucket,
+    messagingSenderId:import.meta.env.VITE_messagingSenderId,
+    appId: import.meta.env.VITE_appId,
 };
 
 const app = initializeApp(firebaseConfig);
--- src/pages/AddToCart.jsx
+++ src/pages/AddToCart.jsx
@@ -1,20 +1,127 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 
 const AddToCart = () => {
     const [data, setData] = useState([]);
 
     useEffect(()=>{
-        fetch('https://glowing-cosmetics-shop-server.vercel.app/addToCart')
+        fetch('http://localhost:5000/addToCart')
         .then(res => res.json())
         .then(data => setData(data))
     },[])
     return (
-        <div>
-            {/*  */}
-            <h2>data {data.length}</h2>
-            
+        <section className='container px-4 mx-auto pt-12'>
+        <div className='flex items-center gap-x-3'>
+          <h2 className='text-lg font-medium text-gray-800 '>My Posted Jobs</h2>
+  
+          <span className='px-3 py-1 text-xs text-blue-600 bg-blue-100 rounded-full '>
+             Job
+          </span>
         </div>
+  
+        <div className='flex flex-col mt-6'>
+          <div className='-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8'>
+            <div className='inline-block min-w-full py-2 align-middle md:px-6 lg:px-8'>
+              <div className='overflow-hidden border border-gray-200  md:rounded-lg'>
+                <table className='min-w-full divide-y divide-gray-200'>
+                  <thead className='bg-gray-50'>
+                    <tr>
+                      <th
+                        scope='col'
+                        className='py-3.5 px-4 font-semibold text-left rtl:text-right text-xl '
+                      >
+                        {/* <div className='flex items-center gap-x-3'>
+                          <span></span>
+                        </div> */}
+                        Product
+                      </th>
+  
+                     
+                      
+  
+                      <th
+                        scope='col'
+                        className='px-4 py-3.5 font-semibold text-xl text-left rtl:text-right '
+                      >
+                        Price
+                      </th>
+                      <th
+                        scope='col'
+                        className='px-4 py-3.5 font-semibold text-xl text-left rtl:text-right '
+                      >
+                        Qty
+                      </th>
+  
+                      <th className='px-4 py-3.5 font-semibold text-xl text-left rtl:text-right '>
+                        Total
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className='bg-white divide-y divide-gray-200 '>
+                    {data.map(job => (
+                      <tr key={job._id}>
+                        <td className='px-4 py-4 text-sm text-gray-500  whitespace-nowrap'>
+                         <div className="flex items-center gap-5">
+                            <img className="size-20" src={job.photo} alt="" />
+                            <h2 className="text-xl font-bold">{job.productName}</h2>
+                         </div>
+                        </td>
+  
+                       
+  
+                        <td className='px-4 py-4 font-semibold  whitespace-nowrap'>
+                          ${job.price}
+                        </td>
+                       
+                       <td>
+                         {/* <input type="number" min="1" value={job.quantity} onChange={(e) => handleQuantityChange(e, job._id)} /> */}
+                         {/* <input type="number" min="1" max={50} className="border-2 p-2 rounded-md"  /> */}
+                         <div className="flex gap-3 border-2 rounded-md w-max px-3 py-2 bg-[#F5F5F5]">
+                              <span className="cursor-pointer">-</span>
+                              <span>{job.quantity}</span>
+                              <span className="cursor-pointer">+</span>
+
+                         </div>
+ 
+                       </td>
+                       <td>
+                          $220
+                       </td>
+                        <td className='px-4 py-4 text-sm whitespace-nowrap'>
+                          <div className='flex items-center gap-x-6'>
+                            <button
+                             
+                              className='text-gray-500 transition-colors duration-200   hover:text-red-500 focus:outline-none'
+                            >
+                              <svg
+                                xmlns='http://www.w3.org/2000/svg'
+                                fill='none'
+                                viewBox='0 0 24 24'
+                                strokeWidth='1.5'
+                                stroke='currentColor'
+                                className='w-5 h-5'
+                              >
+                                <path
+                                  strokeLinecap='round'
+                                  strokeLinejoin='round'
+                                  d='M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0'
+                                />
+                              </svg>
+                            </button>
+  
+                           
+                          </div>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     );
 };
 
